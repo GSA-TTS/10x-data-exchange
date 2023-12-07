c6f2bdff1d7373bdcebb07aee44be6163e9377b4
John Skiles Skinner <john.skinner@gsa.gov>
2023-12-07 09:30:45 -0800

    Same origin / cross origin experiment

--- server-pair/a.py
+++ server-pair/a.py
@@ -5,4 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<html><h1>Flask A</h1><a href="http://localhost:5002">to B</a></html>'
+    return """<!DOCTYPE html>
+        <html>
+            <h1>Flask A</h1>
+            <p>
+                <a href="http://localhost:5002">to B</a>
+            </p>
+            <p>
+                <a href="http://localhost:5002/static/image.jpg" download="image.jpg">
+                    Fetch across orgin
+                </a>
+            </p>
+            </form>
+        </html>"""
--- server-pair/b.py
+++ server-pair/b.py
@@ -1,8 +1,19 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 
 @app.route("/")
 def index():
-    return '<html><h1>Flask B</h1><a href="http://localhost:5001">to A</a></html>'
+    return """<!DOCTYPE html>
+        <html>
+            <h1>Flask B</h1>
+            <p>
+                <a href="http://localhost:5001">to A</a>
+            </p>
+            <p>
+                <a href="static/image.jpg" download="image.jpg">
+                    Same origin
+                </a>
+            </p>
+        </html>"""
