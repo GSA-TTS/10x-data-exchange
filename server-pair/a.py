6fd45f418e928f3978e7e9abad59c86e002576e7
John Skiles Skinner <john.skinner@gsa.gov>
2023-12-07 19:00:08 -0800

    Cross-origin JSON request

--- server-pair/a.py
+++ server-pair/a.py
@@ -1,28 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return """<!DOCTYPE html>
-        <html>
-            <script>
-                async function getData() {
-                    const obj = await fetch("http://localhost:5002/data");
-                    console.log(obj)
-                }
-            </script>
-            <h1>Flask A</h1>
-            <p>
-                <a href="http://localhost:5002">to B</a>
-            </p>
-            <p>
-                <a href="http://localhost:5002/static/image.jpg" download="image.jpg">
-                    Fetch image across orgin
-                </a>
-            </p>
-            <p>
-                <a href="#" onclick="getData()">Data</a>
-            </p>
-        </html>"""
+    return render_template("a.html")
--- server-pair/b.py
+++ server-pair/b.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__, static_url_path="/static")
@@ -7,19 +7,9 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return """<!DOCTYPE html>
-        <html>
-            <h1>Flask B</h1>
-            <p>
-                <a href="http://localhost:5001">to A</a>
-            </p>
-            <p>
-                <a href="static/image.jpg" download="image.jpg">
-                    Same origin image
-                </a>
-            </p>
-        </html>"""
+    return render_template("b.html")
+
 
 @app.route("/data")
 def data():
-    return {"test": ["yes"]}
+    return {"bird": "swan"}
